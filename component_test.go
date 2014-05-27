3ecc595bdcc600fcc76dd46dfc16f1b8bfbb0863
Felix Rath <felix.rath@rwth-aachen.de>
2014-05-28 00:41:13 +0200

    tests now use lil-t

--- component_test.go
+++ component_test.go
@@ -1,6 +1,10 @@
 package es
 
-import "testing"
+import (
+	"testing"
+
+	"github.com/futile/go-lil-t"
+)
 
 type mockComponent struct {
 	BaseComponent
@@ -8,80 +12,46 @@ type mockComponent struct {
 	mockBool bool
 }
 
-var mockComponentFactory ComponentFactory = func() Component {
+func mockComponentFactory() Component {
 	return &mockComponent{mockBool: false}
 }
 
 func TestComponentContainer(t *testing.T) {
+	If := lilt.NewIf(t)
+
 	e := Entity{id: 0, reuseCount: 0}
 
 	cc := NewComponentContainer(mockComponentFactory)
-
-	if cc.Has(e) {
-		t.Errorf("Has() returned true for empty ComponentContainer!")
-	}
+	If(cc.Has(e)).Errorf("Has() returned true for empty ComponentContainer!")
 
 	c, err := cc.Create(e)
-
-	if c == nil {
-		t.Errorf("Create() returned nil!")
-	}
-
-	if err != nil {
-		t.Errorf("Create() returned an error!")
-	}
+	If(c == nil).Errorf("Create() returned nil!")
+	If(err != nil).Errorf("Create() returned an error!")
+	If(!cc.Has(e)).Errorf("Has() returned false even though Component was created!")
 
 	if _, ok := c.(*mockComponent); !ok {
 		t.Errorf("Create() returned wrong type! expected: *mockComponent, but got: %T", c)
 	}
 
-	if !cc.Has(e) {
-		t.Errorf("Has() returned false even though Component was created!")
-	}
-
 	e.reuseCount = 1
-
-	if cc.Has(e) {
-		t.Errorf("Has() returned true even though reuseCount was changed!")
-	}
+	If(cc.Has(e)).Errorf("Has() returned true even though reuseCount was changed!")
 
 	c, _ = cc.Create(e)
-
-	if c.reuseCount() != e.reuseCount {
-		t.Errorf("Wrong reuse counts: entity: %v, component: %v", e.reuseCount, c.reuseCount())
-	}
+	If(c.reuseCount() != e.reuseCount).Errorf("Wrong reuse counts: entity: %v, component: %v", e.reuseCount, c.reuseCount())
 
 	_, err = cc.Create(e)
-
-	if err == nil {
-		t.Errorf("Second call to Create() did not cause an error!")
-	}
+	If(err == nil).Errorf("Second call to Create() did not cause an error!")
 
 	c2 := cc.Get(e)
-
-	if c2 != c {
-		t.Errorf("Get() returned a different Component than Create() did!")
-	}
+	If(c2 != c).Errorf("Get() returned a different Component than Create() did!")
 
 	c2 = cc.GetOrCreate(e)
-
-	if c2 != c {
-		t.Errorf("GetOrCreate() returned a different Component than Create() did!")
-	}
+	If(c2 != c).Errorf("GetOrCreate() returned a different Component than Create() did!")
 
 	err = cc.Remove(e)
-
-	if err != nil {
-		t.Errorf("Remove() falsely returned an error: %v", err)
-	}
-
-	if cc.Has(e) {
-		t.Errorf("Has() returned true after call to Remove()!")
-	}
+	If(err != nil).Errorf("Remove() falsely returned an error: %v", err)
+	If(cc.Has(e)).Errorf("Has() returned true after call to Remove()!")
 
 	err = cc.Remove(e)
-
-	if err == nil {
-		t.Errorf("Second call to Remove() did not return an error!")
-	}
+	If(err == nil).Errorf("Second call to Remove() did not return an error!")
 }
--- world_test.go
+++ world_test.go
@@ -1,32 +1,25 @@
 package es
 
-import "testing"
+import (
+	"testing"
+
+	"github.com/futile/go-lil-t"
+)
 
 func TestWorld(t *testing.T) {
+	If := lilt.NewIf(t)
+
 	world := NewWorld()
 
 	e := world.NewEntity()
 	e2 := world.NewEntity()
-
-	if e.id == e2.id {
-		t.Errorf("Two entities with the same id created!")
-	}
+	If(e.id == e2.id).Errorf("Two entities with the same id created!")
 
 	world.RemoveEntity(e2)
-
 	err := world.RemoveEntity(e2)
-
-	if err == nil {
-		t.Errorf("Double-remove not noticed!")
-	}
+	If(err == nil).Errorf("Double-remove not noticed!")
 
 	e3 := world.NewEntity()
-
-	if e3.id != e2.id {
-		t.Errorf("Id was not reused!")
-	}
-
-	if e3.reuseCount == e2.reuseCount {
-		t.Errorf("reuseCount is the same!")
-	}
+	If(e3.id != e2.id).Errorf("Id was not reused!")
+	If(e3.reuseCount == e2.reuseCount).Errorf("reuseCount is the same!")
 }
